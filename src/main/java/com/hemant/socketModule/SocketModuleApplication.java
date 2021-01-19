165f43fd12d259a44a2eb0894f91560b3ea238c6
hemant <hemant@LAPTOP-IMKH6M8T.mshome.net>
2021-01-19 16:26:49 +0530

    feature() : initial commit

---
+++ src/main/java/com/hemant/socketModule/SocketModuleApplication.java
@@ -0,0 +1,13 @@
+package com.hemant.socketModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hemant/socketModule/SocketModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemant.socketModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
